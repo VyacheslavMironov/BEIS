92559fe6706f51819f9522e6f15a3d1895db2826
VuacheslavMironov <vuacheslav.mir@gmail.com>
2022-04-18 11:43:22 +0700

    Pull data in Json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # !/usr/bin/python 
 # -*- coding: utf-8 -*-
+from hashlib import sha1
 from configparser import ConfigParser
 from flask_httpauth import HTTPBasicAuth
 from flask import (
@@ -12,55 +13,75 @@ from flask import (
     url_for
 )
 
-from models.model import db, Users
+from models.model import db, Users, Messangers, Messagers, Bots
 
 
 
 # ------------------------------------------------
 # Создание таблиц перед запуском приложения
 # ------------------------------------------------
-# db.connect()
-# db.create_tables([Users])
-# db.close()
+db.connect()
+db.create_tables([Users, Messangers, Messagers, Bots])
+db.close()
 # ------------------------------------------------
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    pass
+# @app.route('/', methods=['GET'])
+# def index():
+#     pass
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['POST'])
 def registration():
-    pass
+    if request.method == "POST":
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        token = request.form.get("token")
+        password = request.form.get("password")
+        created_at = request.form.get("created_at")
 
-@app.route('/sigin', methods=['GET', 'POST'])
-def login():
-    pass
+        # Проверка на заполненость данных
+        if name is None or surname is None or password is None:
+            return abort(400)
+        
 
-@app.route('/profile', methods=['GET','POST'])
-def profile():
-    pass
+        return (jsonify(
+            {
+                'name': name, 
+                'surname': surname, 
+                'token': token, 
+                'password': password,
+                'created_at': created_at
+            }
+        ), 200)
 
-@app.route('/bots', methods=['GET'])
-def bot_all():
-    pass
+# @app.route('/sigin', methods=['GET', 'POST'])
+# def login():
+#     pass
 
-@app.route('/bot-create', methods=['GET', 'POST'])
-def bot_create():
-    pass
+# @app.route('/profile', methods=['GET','POST'])
+# def profile():
+#     pass
 
-@app.route('/bots-info', methods=['GET'])
-def bot_info():
-    pass
+# @app.route('/bots', methods=['GET'])
+# def bot_all():
+#     pass
 
-@app.route('/bots-drop', methods=['GET', 'DELETE'])
-def bot_drop():
-    pass
+# @app.route('/bot-create', methods=['GET', 'POST'])
+# def bot_create():
+#     pass
 
-@app.route('/bots-update', methods=['GET', 'PUT'])
-def bot_update():
-    pass
+# @app.route('/bots-info', methods=['GET'])
+# def bot_info():
+#     pass
+
+# @app.route('/bots-drop', methods=['GET', 'DELETE'])
+# def bot_drop():
+#     pass
+
+# @app.route('/bots-update', methods=['GET', 'PUT'])
+# def bot_update():
+#     pass
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- models/model.py
+++ models/model.py
@@ -7,7 +7,8 @@ from peewee import (
     Model, 
     CharField,
     BigIntegerField,
-    DateField
+    DateField,
+    ForeignKeyField
 )
 
 
@@ -26,12 +27,56 @@ db = MySQLDatabase(
 class Users(Model):
     id = BigIntegerField(primary_key=True, unique=True,
                         constraints=[SQL('AUTO_INCREMENT')])
-    name = CharField()
-    surname = CharField()
-    token = CharField()
+    name = CharField(max_length=30)
+    surname = CharField(max_length=35)
+    token = CharField(max_length=500)
+    password = CharField(max_length=500)
     created_at = DateField(default=datetime.now())
 
     class Meta:
         database = db # This model uses the "people.db" database.
 
 
+class Messangers(Model):
+    id = BigIntegerField(primary_key=True, unique=True,
+                        constraints=[SQL('AUTO_INCREMENT')])
+    title = CharField(max_length=25)
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+
+class Messagers(Model):
+    id = BigIntegerField(primary_key=True, unique=True,
+                        constraints=[SQL('AUTO_INCREMENT')])
+    user_ids = CharField(max_length=255)
+    chat_name = CharField(max_length=120, default='')
+    username = CharField(max_length=90, default='')
+    name = CharField(max_length=90, default='')
+    surname = CharField(max_length=90, default='')
+    text = CharField(max_length=500, default='')
+    img = CharField(max_length=180, default='')
+    video = CharField(max_length=180, default='')
+    voice = CharField(max_length=1000, default='')
+    voice_text = CharField(max_length=1000, default='')
+    file = CharField(max_length=180, default='')
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+
+class Bots(Model):
+    id = BigIntegerField(primary_key=True, unique=True,
+                        constraints=[SQL('AUTO_INCREMENT')])
+    user_id = ForeignKeyField(Users, backref='id')
+    messenger_id = ForeignKeyField(Messangers, backref='id')
+    # message_id = ForeignKeyField(Messangers, backref='id')
+    name = CharField(max_length=30)
+    recipient = CharField(max_length=50)
+    token = CharField(max_length=255, unique=True)
+    app_token = CharField(max_length=255, unique=True)
+    hash_token = CharField(max_length=255, unique=True)
+    created_at = DateField(default=datetime.now())
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
